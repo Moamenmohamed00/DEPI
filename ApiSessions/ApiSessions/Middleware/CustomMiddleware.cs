7138928835319f17e4a55b8fe9aaa59ae43482ca
Moamen Mohamed <125478677+Moamenmohamed00@users.noreply.github.com>
2026-03-01 15:27:35 +0200

    Add files via upload

---
+++ ApiSessions/ApiSessions/Middleware/CustomMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace ApiSessions.Middleware
+{
+    public class CustomMiddleware
+    {
+        //middleware is inline-custom middleware(class or inherit from imiddleware)
+        private readonly RequestDelegate _next;
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine(">>>Custom Middleware: Before next middleware");
+            await _next(context);
+            Console.WriteLine("<<<Custom Middleware: After next middleware");
+        }
+    }
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
---
+++ ApiSessions/ApiSessions/Middleware/TestMiddleware.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ApiSessions.Middleware
+{
+    public class TestMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate _next)
+        {
+            
+            Console.WriteLine(">>>Test Middleware: Before next middleware");
+            await _next(context);
+            Console.WriteLine("<<<Test Middleware: After next middleware");
+        }
+    }
+    public static class TestMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseTestMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TestMiddleware>();
+        }
+    }
+}
